a77e91e05385fdc83fdc437866458cde555c23b4
DIANAROSARIO29 <dianarosario2911@gmail.com>
2023-11-14 21:44:30 -0500

    v2 update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+////style={{background:"violet",width:"350px",padding:"10px"}}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{background:"violet",width:"350px",padding:"10px"}}>
+     <hi>App.js</hi> 
+     
     </div>
   );
 }
---
+++ src/protegido/AppForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AppForm = () => {
+  return (
+    <div>
+      <h1>AppForm.js</h1>
+      Pagina con formulario...
+    </div>
+  )
+}
+
+export default AppForm
